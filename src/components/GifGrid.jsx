2fe870c5ff8cf0c3fcdbd7e7e707cba5bcfb4aef
jamiechwr <jamiechwr97@gmail.com>
2022-08-20 12:23:19 +0200

    Mostrar los GIF y sus títulos.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-    getGifs(category);
+    const [images, setImages] = useState([]);
+
+    useEffect( () => {
+        getGifs(category).then(
+            newImages => setImages(newImages)
+        );
+    }, [])
 
     return (
         <>
             <h3>{ category }</h3>
+
+            <div className="card-grid">
+                {
+                    images.map( (image) => (
+                        <GifItem 
+                            key={ image.id }
+                            { ...image }
+                        />
+                    )) 
+                }
+            </div>
         </>
     )
 }
